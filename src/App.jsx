5e4d28f36fe8b6223ad352ba27a0946c4918c6f2
jordelucas <jordevalucas@hotmail.com>
2022-08-07 15:03:13 -0300

    add sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import { Header } from './components/Header'
-import './globals.css'
+import { Post } from './components/Post';
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+
+import './globals.css';
+
+import styles from './App.module.css';
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Hello World!</h1>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Jordevá Lucas"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga excepturi totam voluptate enim necessitatibus et voluptates quasi eveniet quas dolor, dolorem numquam explicabo asperiores cupiditate praesentium fugit temporibus tempora voluptatem?"
+          />
+          <Post 
+            author="ketyllem Costa"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga excepturi totam voluptate enim necessitatibus et voluptates quasi eveniet quas dolor, dolorem numquam explicabo asperiores cupiditate praesentium fugit temporibus tempora voluptatem?"
+          />
+        </main>
+      </div>
     </div>
   )
 }
