2d9ac771d24367537eb86f89b46b3aa4e0a7504c
jordelucas <jordevalucas@hotmail.com>
2022-08-07 19:59:40 -0300

    delete comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,7 +4,11 @@ import { Avatar } from './Avatar';
 
 import styles from './Comment.module.css';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar
@@ -20,7 +24,7 @@ export function Comment({ content }) {
               <time title='11 de Maio às 08:13' dateTime='2022-05-11 )8:13:30'>Cerca de 1h atrás</time>
             </div>
 
-            <button title='Deletar comentário'>
+            <button title='Deletar comentário' onClick={handleDeleteComment}>
               <Trash size={24} />
             </button>
           </header>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,6 +33,14 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value);
   }
 
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(comment => {
+      return comment !== commentToDelete
+    });
+
+    setComments(commentsWithoutDeletedOne)
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -76,7 +84,11 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => (
-          <Comment key={comment} content={comment} />
+          <Comment
+            key={comment}
+            content={comment}
+            onDeleteComment={deleteComment}
+          />
         ))}
       </div>
     </article>
