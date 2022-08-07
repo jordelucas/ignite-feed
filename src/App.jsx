b5cadc2d90b90adcc657ce0ac0664086a45c44bb
jordelucas <jordevalucas@hotmail.com>
2022-08-07 19:01:38 -0300

    iterate through the posts

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import './globals.css';
 
 import styles from './App.module.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Instructor @ Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-08-03 20:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @ Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-08-07 15:00'),
+  },
+]
+
 function App() {
   return (
     <div>
@@ -14,14 +45,14 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Jordevá Lucas"
-            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga excepturi totam voluptate enim necessitatibus et voluptates quasi eveniet quas dolor, dolorem numquam explicabo asperiores cupiditate praesentium fugit temporibus tempora voluptatem?"
-          />
-          <Post 
-            author="ketyllem Costa"
-            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga excepturi totam voluptate enim necessitatibus et voluptates quasi eveniet quas dolor, dolorem numquam explicabo asperiores cupiditate praesentium fugit temporibus tempora voluptatem?"
-          />
+          {posts.map(post => (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </div>
