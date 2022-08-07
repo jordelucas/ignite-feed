851ee7d568e49746fb24c0e341a53ae1f944b3e5
jordelucas <jordevalucas@hotmail.com>
2022-08-07 11:53:58 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from './components/Header'
+import './globals.css'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
